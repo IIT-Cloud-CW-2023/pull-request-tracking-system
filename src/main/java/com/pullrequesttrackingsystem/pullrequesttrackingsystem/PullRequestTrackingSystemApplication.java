c90134b88d7e2bf92819a4bd1d1595a0b6e07a2c
hiranjanperera7532 <hiranjanperera7532@gmail.com>
2023-12-05 16:19:49 +0530

    INITIAL COMMIT

---
+++ src/main/java/com/pullrequesttrackingsystem/pullrequesttrackingsystem/PullRequestTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pullrequesttrackingsystem.pullrequesttrackingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PullRequestTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PullRequestTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pullrequesttrackingsystem/pullrequesttrackingsystem/PullRequestTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pullrequesttrackingsystem.pullrequesttrackingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PullRequestTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
